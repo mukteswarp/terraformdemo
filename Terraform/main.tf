2977a584f9c3c2e8bf788bef52536d4a41d1f1c6
Mukteswar Patnaik <mukteswar@msn.com>
2021-04-26 09:39:49 +0530

    Auto Build Testing

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -9,6 +9,7 @@ terraform {
     }
 }
 
+
 provider "azurerm" {
     # The "feature" block is required for AzureRM provider 2.x.
     # If you're using version 1.x, the "features" block is not allowed.
